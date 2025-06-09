62e7223da111b2f14db78d07dd48d22a322aa3ef
sinchan143054 <sinchan.143054@gmail.com>
2025-06-09 09:17:03 +0530

    save

---
+++ Number_type.java
@@ -0,0 +1,13 @@
+public class Number_type {
+    public static void main(String[] args) {
+        int n=10;
+        if(n>0){
+            System.out.print("positive");
+        }
+        else{
+            System.out.print("negative or zero");
+        }
+    
+    }
+
+}
