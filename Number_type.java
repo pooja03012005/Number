9adf081d2e26cd7117d4c135d5f5c5bef72f5c2f
sinchan143054 <sinchan.143054@gmail.com>
2025-06-09 09:56:57 +0530

    save changes

--- Number_type.java
+++ Number_type.java
@@ -1,11 +1,11 @@
 public class Number_type {
     public static void main(String[] args) {
         int n=10;
-        if(n>0){
-            System.out.print("positive");
+        if(n==0){
+            System.out.print("zero"+n);
         }
-        else{
-            System.out.print("negative or zero");
+        else if(n<0){
+            System.out.print("negative"+n);
         }
     
     }
