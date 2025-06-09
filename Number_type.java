c76528f3d55ba39156d8eb2a4544a6f1baa605fa
Pooja HM <poojamadiwalar449@gmail.com>
2025-06-09 10:20:32 +0530

    Update Number_type.java

--- Number_type.java
+++ Number_type.java
@@ -1,13 +1,13 @@
 public class Number_type {
     public static void main(String[] args) {
-        int n=10;
-        if(n==0){
-            System.out.print("zero");
-        }
-        else(n>0){
-            System.out.print("positive");
-        }
-    
-    }
+        int n = 10;
 
+        if (n == 0) {
+            System.out.print("zero");
+        } else if (n > 0) {
+            System.out.print("positive");
+        } else {
+            System.out.print("negative");
+        }
+    }
 }
